312c0285f871a6d6d2b7050ebf1a0ef95b39955c
adem-keskin <ademkeskin.de@gmail.com>
2024-01-05 21:47:32 +0100

    01.01.2024

---
+++ src/test/java/com/myfirstproject/Day01_FirstSeleniumClass.java
@@ -0,0 +1,14 @@
+package com.myfirstproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Day01_FirstSeleniumClass {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
---
+++ src/test/java/com/myfirstproject/Day10_ScreenshotOfSpecificElements.java
@@ -0,0 +1,34 @@
+package com.myfirstproject;
+
+import com.myfirstproject.utilities.TestBase;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class Day10_ScreenshotOfSpecificElements extends TestBase {
+    @Test
+    public void screenshotTest() throws IOException {
+        driver.get("https://www.google.com");
+        WebElement googleSearchLogo = driver.findElement(By.xpath("(//input[@class='gNO89b'])[2]"));
+        File logoImage = googleSearchLogo.getScreenshotAs(OutputType.FILE);
+//        Saving the image to  the path
+        String currentDate = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        File finalPath = new File("./test-output/elements/googleSearchLogo"+currentDate+".png");
+        FileUtils.copyFile(logoImage,finalPath);
+
+
+        /***********************************************************************************************************/
+//        getting screenshot of sign in element
+        WebElement signInButton = driver.findElement(By.xpath("//*[.='Sign in']"));
+        File signInImage = signInButton.getScreenshotAs(OutputType.FILE);
+        String currentDate2 = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        File signInPath = new File("./test-output/elements/signIn"+currentDate2+".png");
+        FileUtils.copyFile(signInImage,signInPath);
+    }
+}
